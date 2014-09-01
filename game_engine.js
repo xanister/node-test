0dace884c2c8f1e3572593e300643986db963154
Nicholas Frees <xanister@hotmail.com>
2014-09-01 23:40:19 +0000

    ...

--- game_engine.js
+++ game_engine.js
@@ -1,13 +1,15 @@
-var gs = new GameClient();
+$(document).ready(function() {
+    var gs = new GameClient();
 
-gs.init();
+    gs.init();
 
-setInterval(function() {
-    $.each(window.uos, function(id, uo) {
-        if ($('#' + id).length === 0) {
-            $('body').append("<div class='uo' id='" + id + "'></div>");
-        }
-        $('#' + id).css('left', uo.x + "px");
-        $('#' + id).css('top', uo.y + "px");
-    });
-}, 33);
+    setInterval(function() {
+        $.each(gs.uos, function(id, uo) {
+            if ($('#' + id).length === 0) {
+                $('body').append("<div class='uo' id='" + id + "'></div>");
+            }
+            $('#' + id).css('left', uo.x + "px");
+            $('#' + id).css('top', uo.y + "px");
+        });
+    }, 33);
+});
